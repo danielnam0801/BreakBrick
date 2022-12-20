0927bf7b0c919e83320241aee52eae5b1b7ea24c
danielnam0801 <danielnam0801@gmail.com>
2022-12-20 23:52:41 +0900

    만드는중

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -7,26 +7,31 @@ public class Ball : MonoBehaviour
     Rigidbody2D _rigid;
     [SerializeField] private float xSpeed = 1f;
     [SerializeField] private float ySpeed = 1f;
+    
     public LayerMask canCollisionlayer;
+
+    Vector3 lastVelocity;
+    float rotateValue;
 
     private void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
-        _rigid.velocity = -new Vector2(xSpeed, ySpeed);   
+        rotateValue = Random.Range(-1f, 1f);    
+        _rigid.velocity = -new Vector2(xSpeed * rotateValue, ySpeed);
     }
 
     private void Update()
     {
-    
+        lastVelocity = _rigid.velocity;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.layer == canCollisionlayer)
         {
-            Vector2 InDir = _rigid.velocity;
-            Vector2 InNormal = collision.contacts[0].normal;
-            _rigid.velocity = Vector2.Reflect(InDir, InNormal);
+            float speed = lastVelocity.magnitude;
+            Vector2 dir = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+            _rigid.velocity = dir * Mathf.Max(speed, 0f);
         }
     }
 }
